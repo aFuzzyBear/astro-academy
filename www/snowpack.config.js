9e019f161028e8b00228188d47fbb4bfe4d50502
aFuzzyBear <drgaud@hotmail.com>
2021-09-14 19:01:04 +0100

    added aliases to the snowpack engine

---
+++ www/snowpack.config.js
@@ -0,0 +1,27 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  alias:{
+    Pages:'./src/pages',
+    Components:'./src/components',
+    Layouts:'./src/layouts',
+    Data:'./src/data'
+  },
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
